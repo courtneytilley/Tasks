931e5046db77f630a14bde4109f852a3dac63364
courtneytilley <cnt0005@mix.wvu.edu>
2021-02-05 10:41:28 -0800

    courtney tilley Task 02 upload b

--- Task_02/task02.r
+++ Task_02/task02.r
@@ -25,4 +25,46 @@ head(Data)
 beren2 <- Data
 beren3 <- beren2[order(beren2$age) ,]
 write.csv(beren3, 'beren_new.csv', quote=F, row.names=FALSE)
-
+Feeds <- which(beren3$event == "bottle")
+avgMilk <- mean(beren3$value[Feeds]
+beren <- read.csv("http://jonsmitchell.com/data/beren.csv", stringsAsFactors=F)
+dayID <- apply(beren, 1, function(x) paste(x[1:3], collapse="-"))
+dateID <- sapply(dayID, as.Date, format = "%Y-%m-%d", origin = "2019-04-18")
+beren$age <- dateID - dateID[which(beren$event == "birth")]
+beren2 <- beren[order(beren$age),]
+beren2$value <- as.numeric(beren2$value)
+beren3 <- beren2
+Feeds <- which(beren3$event == "bottle")
+avgMilk <- mean(beren3$value[Feeds])
+avgMilk
+The unit for avgMilk is ounces.The value column was used to show the numerical value that went with the "Feeds" data. 
+avgFeed <- tapply(beren3$value[Feeds], beren3$age[Feeds], mean)
+avgFeed
+varFeed <- tapply(beren3$value[Feeds], beren3$age[Feeds], var)
+totalFeed <- tapply(beren3$value[Feeds], beren3$age[Feeds], sum)
+numFeeds <- tapply(beren3$value[Feeds], beren3$age[Feeds], length)
+cor(beren3$value[Feeds], beren3$age[Feeds])
+cor.test(beren3$value[Feeds], beren3$age[Feeds])
+berenCor <- cor.test(beren3$value[Feeds], beren3$age[Feeds])
+beren3$age[Feeds]
+summary(berenCor)
+berenANOVA <- aov(beren3$value[Feeds] ~ beren3$caregiver[Feeds])
+boxplot(beren3$value[Feeds] ~ beren3$caregiver[Feeds], xlab = "who gave the bottle", ylab = "amount of milk consumed (oz)")
+?par
+las = edited axis labels
+mar = margins that should be used 
+mgp = margins that should be used for title and axis 
+tck = edited length of tick marks
+par(las=1, mar=c(5, 5, 1, 1), mgp=c(2, 0.5, 0), tck=-0.01)
+plot(as.numeric(names(totalFeed)), totalFeed, type="b", pch=16, xlab= "age in days", ylab= "ounces of milk")
+abline(h=mean(totalFeed), lty=2, col='red')
+pdf("r02b-totalMilkByDay.pdf", height=4, width=4)
+par(las=1, mar=c(5, 5, 1, 1), mgp=c(2, 0,5, 0), tck=-0.01)
+plot(as.numeric(names(totalFeed)), totalFeed, type="b", pch=16, xlab="age in days", ylab="ounces of milk")
+abline(h=mean(totalFeed), lty=2, col='red')
+dev.off()
+beren4 <- beren3[Naps,]
+startHour <- (beren4$start_hour)
+startMin <- (beren4$star_minute)
+stopHour <- (beren4$end_hour)
+stopMin <- (beren4$end_minute)
