2268b51c90d990c85591b7e04168f2f7815a7054
courtneytilley <cnt0005@mix.wvu.edu>
2021-04-30 01:40:20 -0700

    courtney tilley task 11 upload

--- Task_11/task11.r
+++ Task_11/task11.r
@@ -9,3 +9,19 @@ coef(lm(y~x))
 y intercept = 2.193123 x intercept = 4.989395
 z <- c()
 x <- rnorm(100, mean=5, sd=2)
+for(i in 1:100) {
+  z[i]<-runif(1)
+  y<- (x* z[i]) + 2 +(rnorm(100, 0:0.1))
+  l<-coef(lm(z[1:100]~y))
+}
+pdf("2.pdf", height=4, width=4)
+plot(z[1:100], y)
+abline(lm(y~z[1:100]))
+dev.off()
+pdf("3.pdf", height=4, width=4)
+plot(c(z, -0.03))
+dev.off()
+install.packages('meme')
+library('meme')
+u <-https://www.pinterest.com/pin/225602262557834822/
+my_meme <- meme(u, lower= "when you get error again after the 10th try", color="white", size="2.0")  
